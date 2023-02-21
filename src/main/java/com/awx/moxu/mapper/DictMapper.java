a2e00278255a3ba000e7f0e40734bdb1dab9d148
安丽航 <awx1030@126.com>
2023-02-21 18:51:36 +0800

    字典服务实体及vo

---
+++ src/main/java/com/awx/moxu/mapper/DictMapper.java
@@ -0,0 +1,17 @@
+package com.awx.moxu.mapper;
+
+import com.awx.moxu.entity.Dict;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Entity com.awx.moxu.entity.Dict
+ */
+@Mapper
+public interface DictMapper extends BaseMapper<Dict> {
+
+}
+
+
+
+
